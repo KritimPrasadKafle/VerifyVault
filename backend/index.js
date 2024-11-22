0defc5bed757b2fd3fadd9f17d2feb4a64983f19
KritimPrasadKafle <kritim10k@gmail.com>
2024-11-22 22:50:39 +0545

    Connection to the DB

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { connectDB } from './db/connectDB.js';
+import dotenv from 'dotenv';
+const app = express()
+
+dotenv.config();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  connectDB();
+  console.log('Server is running on port 3000')
+})
+
+
